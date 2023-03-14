448c49ba792ad465188abf0220acd55d87efe093
sawissac <issacmm64@gmail.com>
2023-03-14 14:15:46 +0630

    upadtate

--- src/components/ContextTest/Child.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { ThemeContext } from "./ThemeContext";
-
-export function Child() {
-  const globalState = React.useContext(ThemeContext);
-
-  return <div>{globalState.mode}</div>;
-}
